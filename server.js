c21fac442ba5b5b48f4cc1076647272a4fc315fd
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-05-08 17:46:13 -0600

    added express, middleware, port, sql

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
 const inquirer = require('inquirer');
+const express = require('express');
+// Import and require mysql2
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 function showTable() {
   
