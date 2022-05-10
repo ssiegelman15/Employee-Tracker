0410d13918c2316ccd8285b9a6d043564c91196e
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-05-09 20:12:48 -0600

    created array of questions to be prompted based on choice from initialPrompt

--- helpers/questions.js
+++ helpers/questions.js
@@ -7,4 +7,71 @@ const initialPrompt = [
   }
 ];
 
-module.exports = {initialPrompt};
+const addDepartment = [
+  {
+    type: "input",
+    message: "What is the name of the department that you would like to add?",
+    name: "addDepartment"
+  }
+];
+
+const addRole = [
+  {
+    type: "input",
+    message: "What is the name of the role that you would like to add?",
+    name: "addRole"
+  },
+  {
+    type: "input",
+    message: "What is the salary for this role?",
+    name: "roleSalary"
+  },
+  {
+    type: "list",
+    message: "What department does this role report to?",
+    choices: ["NEED TO UPDATE THIS TO SHOW ALL DEPARTMENTS", "AND HAVE IT ABLE TO READ THE UP TO DATE DEPT ARRAY"],    
+    name: "roleDepartment"
+  }
+];
+
+const addEmployee = [
+  {
+    type: "input",
+    message: "What is the new employee's first name?",
+    name: "firstName"
+  },
+  {
+    type: "input",
+    message: "What is the new employee's last name?",
+    name: "lastName"
+  },
+  {
+    type: "list",
+    message: "What role will the new employee have?",
+    choices: ["NEED TO UPDATE THIS TO SHOW ALL ROLES", "AND HAVE IT ABLE TO READ THE UP TO DATE ROLES ARRAY"],    
+    name: "employeeRole"
+  },
+  {
+    type: "list",
+    message: "Who will the new employee's manager be?",
+    choices: ["NEED TO UPDATE THIS TO SHOW ALL EMPLOYEES", "AND HAVE IT ABLE TO READ THE UP TO DATE EMPLOYEE ARRAY", "WITH NONE AS FIRST OPTION"],    
+    name: "employeeManager"
+  }
+];
+
+const updateRole = [
+  {
+    type: "list",
+    message: "Which employee's role would you like to update?",
+    choices: ["NEED TO UPDATE THIS TO SHOW ALL EMPLOYEES", "AND HAVE IT ABLE TO READ THE UP TO DATE EMPLOYEE ARRAY"],    
+    name: "employeeRoleUpdate"
+  },
+  {
+    type: "list",
+    message: "What role would you like to assign?",
+    choices: ["NEED TO UPDATE THIS TO SHOW ALL ROLES", "AND HAVE IT ABLE TO READ THE UP TO DATE ROLES ARRAY"],    
+    name: "newRole"
+  }
+]
+
+module.exports = {initialPrompt, addDepartment, addRole, addEmployee, updateRole};
