8a8955759081a0472d2f7b5021699f25913b047d
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-05-09 20:24:11 -0600

    seeded roles

--- db/seeds.sql
+++ db/seeds.sql
@@ -8,4 +8,15 @@ VALUES ("Engineering"),
        ("Production");
 
 INSERT INTO roles (title,departmentId,salary)
-VALUES 
+VALUES ("Project Engineer", 1, 100000),
+       ("Engineering Intern", 1, 20000),
+       ("Junior Accountant", 2, 55000),
+       ("HR Business Partner", 4, 75000),
+       ("Maintenance Manager", 3, 125000),
+       ("Senior Accountant", 2, 95000),
+       ("Production Manager", 5, 150000),
+       ("Maintenance Supervisor", 3, 95000),
+       ("Maintenance Technician", 3, 75000),
+       ("Machine Operator", 5, 68000),
+       ("Reliability Engineer", 1, 90000);
+
